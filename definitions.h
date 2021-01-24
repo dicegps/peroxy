7361ecf8f3d24b098473374423bc5f7263528c2e
dicegps <77773212+dicegps@users.noreply.github.com>
2021-01-24 04:43:49 -0500

    Add files via upload

--- definitions.h
+++ definitions.h
@@ -3,4 +3,6 @@
 #include <QString>
 
 #endif // DEFINITIONS_H
-QString possibleElements = ",H-2,He-1,Li-2,Be-1,B-2,C-3,N-4,O-3,F-2,Ne-1,Na-2,Mg-1,Al-2,Si-3,P-4,S-3,Cl-2,Ar-1,K-2,Ca-1,Sc-2,Ti-3,V-4,Cr-5,Mn-6,Fe-5,Co-4,Ni-3,Cu-2,Zn-1,Ga-2,Ge-3,As-4,Se-3,Br-2,Kr-1,Rb-2,Sr-1,Y-2,Zr-3,Nb-4,Mo-5,Tc-6,Ru-5,Rh-4,Pd-3,Ag-2,Cd-1,In-2,Sn-3,Sb-4,Te-3,I-2,Xe-1,Cs-2,Ba-1,La-2,Ce-3,Pr-4,Nd-5,Pm-6,Sm-6,Eu-6,Gd-6,Tb-6,Dy-5,Ho-4,Er-3,Tm-2,Yb-1,Lu-2,Hf-3,Ta-4,W-5,Re-6,Os-5,Ir-4,Pt-3,Au-2,Hg-1,Tl-2,Pb-3,Bi-4,";
+QString possibleElements = ",H-2,Li-2,B-2,C-3,N-4,O-3,F-2,Na-2,Al-2,Si-3,P-4,S-3,Cl-2,K-2,Sc-2,Ti-3,V-4,Cr-5,Mn-6,Fe-5,Co-4,Ni-3,Cu-2,Ga-2,Ge-3,As-4,Se-3,Br-2,Rb-2,Y-2,Zr-3,Nb-4,Mo-5,Tc-6,Ru-5,Rh-4,Pd-3,Ag-2,In-2,Sn-3,Sb-4,Te-3,I-2,Cs-2,La-2,Ce-3,Pr-4,Nd-5,Pm-6,Sm-6,Eu-6,Gd-6,Tb-6,Dy-5,Ho-4,Er-3,Tm-2,Hf-3,Ta-4,W-5,Re-6,Os-5,Ir-4,Pt-3,Au-2,Tl-2,Pb-3,Bi-4,";
+
+//Be,Mg,Ca,Sr,Ba,Zn,Cd,Hg,He,Ne,Ar,Kr,Xe
